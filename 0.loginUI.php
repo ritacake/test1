5b6f509ceb2a1f5b94f06b718f6ffe4e61f94651
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2021-11-14 02:01:24 +0800

    60:00

--- 0.loginUI.php
+++ 0.loginUI.php
@@ -1,10 +1,16 @@
 <?php
-    session_start(); //�ҥ�session �\��, �����bphp�{���٨S��X����T�����e�ҥ�
-    $_SESSION["userID"] = ""; //�ŧisession �ܼƨë��w��
+    session_start(); //啟用session 功能, 必須在php程式還沒輸出任何訊息之前啟用
+
+    // 進來前，都切成未登入狀態
+    $_SESSION["userID"] = ""; // 宣告session 變數並指定值
+    // userID 可以自己取
 ?>
 <hr>
+<!-- 生成一個表單，送到 0.login.php 裡 -->
 <form method="post" action="0.login.php">
 ID <input type="text" name="id"> <br>
+
+<!-- Password 的 type 可以設成 password，讓別人看不到 -->
 Password <input type="text" name="pwd"> <br>
 <input type="submit">
 </form>
