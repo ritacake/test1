e8d2106bc52f4702946119fd827b2b9b92c51361
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2021-12-13 11:40:03 +0800

    todoList Ajax version

--- todo/dbconfig.php
+++ todo/dbconfig.php
@@ -5,8 +5,8 @@ session_start();
 */
 $host = '127.0.0.1'; //執行DB Server 的主機
 $user = 'root'; //登入DB用的DB 帳號
-$pass = 'aiden891102'; //登入DB用的DB 密碼
-$dbName = '2021-1025-test'; //使用的資料庫名稱
+$pass = ''; //登入DB用的DB 密碼
+$dbName = 'se'; //使用的資料庫名稱
 /* $db 即為未來執行SQL指令所使用的物件 */
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 
