5b1895b8249dceb2bb36d17c30df8831598f5f50
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-01-08 22:34:06 +0800

    Model Test Example

--- autoTesting Sample/ModelTest.php
+++ autoTesting Sample/ModelTest.php
@@ -1,37 +1,41 @@
 <?php
+// 用 ModelTest.php 去測試 model.php
 require("model.php");
 echo "test getAvg\n";
+
+//  定義 testcase
 $testcase=[
-	//[x, y, expected]
-	[0,2,1],
-	[1,2,1],
-	[1,1,1]
+    // [x, y, expected]
+    // [輸入的第一個值, 輸入的第二個值, 預期得到的數值]
+    [0,2,1],
+    [1,2,1],
+    [1,1,1]
 ];
 
 foreach ($testcase as $t) {
-	echo "testing {$t[0]}, {$t[1]}";
-	if (getAvg($t[0], $t[1]) == $t[2]) {
-		echo "... pass\n";
-	} else {
-		echo "... failed\n";		
-	}
+    echo "testing {$t[0]}, {$t[1]}";
+    if (getAvg($t[0], $t[1]) == $t[2]) {
+        echo "... pass\n";
+    } else {
+        echo "... failed\n";		
+    }
 }
 
 echo "test getMax\n";
 $testcase=[
-	//[x, y, expected]
-	[0,2,2],
-	[1,1,1],
-	[3,1,3]
+    //[x, y, expected]
+    [0,2,2],
+    [1,1,1],
+    [3,1,3]
 ];
 
 foreach ($testcase as $t) {
-	echo "testing {$t[0]}, {$t[1]}";
-	if (getMax($t[0], $t[1]) == $t[2]) {
-		echo "... pass\n";
-	} else {
-		echo "... failed\n";		
-	}
+    echo "testing {$t[0]}, {$t[1]}";
+    if (getMax($t[0], $t[1]) == $t[2]) {
+        echo "... pass\n";
+    } else {
+        echo "... failed\n";		
+    }
 }
 
 ?>
--- autoTesting Sample/TestControl.php
+++ autoTesting Sample/TestControl.php
@@ -2,17 +2,17 @@
 require("model.php");
 $act=$_POST['act'];
 switch($act) {
-	case "avg":
-		$a = (int)$_POST['v1'];
-		$b = (int)$_POST['v2'];
-		echo getAvg($t[0], $t[1]);
-		break;
-	case "max":
-		$a = (int)$_POST['v1'];
-		$b = (int)$_POST['v2'];
-		echo getMax($t[0], $t[1]);
-		break;
-	default:
-		echo "nothing to do";
+    case "avg":
+        $a = (int)$_POST['v1'];
+        $b = (int)$_POST['v2'];
+        echo getAvg($t[0], $t[1]);
+        break;
+    case "max":
+        $a = (int)$_POST['v1'];
+        $b = (int)$_POST['v2'];
+        echo getMax($t[0], $t[1]);
+        break;
+    default:
+        echo "nothing to do";
 }
 ?>
