9ce69a4dc5084845671dded196c15a6e2fb4ec09
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-01-10 01:47:19 +0800

    teacher version

--- bomb/bomb.php
+++ bomb/bomb.php
@@ -1,16 +1,17 @@
 <?php
  require "dbconnect.php";
-
-
  ?>
 <html><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="text/javascript" src="jquery.js"></script>
 
 <script language="javascript">
+	// new 一個 Audio 物件，要放甚麼音樂
 var audio = new Audio('Explosion+3.mp3');
+// 錢
 var money=1000;
 
+// 設置炸彈
 function setBomb(bombID) {
 	now= new Date(); //get the current time
 	tday=new Date(myArray[bombID]['expire'])
@@ -41,25 +42,35 @@ function setBomb(bombID) {
 	}
 }
 
+// 檢查炸彈
 function checkBomb() {
+	// 更新錢的顯示數量
 	$("#money").html(money);
 	now= new Date(); //get the current time
 	
 	//check each bomb with a for loop
 	//array length: number of items in the global array: myArray
+	// myArray 目前有幾顆炸彈
 	for (i=0; i < myArray.length;i++) {	
+		// expire 取得炸彈爆炸時間
 		tday=new Date(myArray[i]['expire']); //convert the date string into date object in javascript
+		// 判斷到期了沒
 		if (tday <= now) { 
 			//expired, set the explode image and text
+			// 把圖改成爆炸的圖
 			$("#bomb" + i).attr('src',"images/explode.jpg");
+			// 把說明文字，改成 exploded! (表示已爆炸)
 			$("#timer"+i).html("exploded!")
+			// 防呆
 			if (now - tday <= 1500 ) {
 				audio.play();
 				money += 600
 			}
 		} else {
 			//set the bomb image  and calculate count down
+			// 設成炸彈圖
 			$("#bomb" + i).attr('src',"images/bomb.jpg");
+			// 顯示倒數
 			$("#timer"+i).html(Math.floor((tday-now)/1000))			
 		}
 	}
@@ -68,6 +79,7 @@ function checkBomb() {
 //javascript, to set the timer on windows load event
 window.onload = function () {
 	//check the bomb status every 1 second
+	// 每 1 秒鐘去檢查一次炸彈
     setInterval(function () {
 		checkBomb()
     }, 1000);
@@ -81,12 +93,15 @@ Money:<div id="money"></div><hr>
 
 $i=0; //counter for bombs
 $sql="select * from game"; //select all bomb information from DB
+
+// $res 抓 炸彈資料
 $res=mysqli_query($conn,$sql) or die("db error");
 $arr = array(); //define an array for bombs
 
 while($row=mysqli_fetch_assoc($res)) {
 	$arr[] = $row; //store the row into the array
 	//generate the image tag, the div tag for timer text. Note on the use of $i in tag ID
+	// 放炸彈圖
 	echo "<img id='bomb$i' onclick='setBomb($i)'><div id='timer$i'></div><br />";
 	$i++; //increase counter
 }
@@ -96,6 +111,7 @@ while($row=mysqli_fetch_assoc($res)) {
 <script>
 <?php
 	//print the bomb array to the web page as a javascript object
+	// myArray 炸彈資料
 	echo "var myArray=" . json_encode($arr);
 ?>
 </script>
--- guestbook/listMessage.php
+++ guestbook/listMessage.php
@@ -2,34 +2,57 @@
 session_start();
 require("dbconnect.php");
 $sql = "select * from guestbook;";
-$result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
+$result = mysqli_query($conn, $sql) or die("DB Error: Cannot retrieve message.");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- <meta http-equiv="refresh" content="1"> -->
+    <title>無標題文件</title>
 </head>
 
 <body>
 
-<p>my guest book !! <a href='addMessageForm.php' target="_blank">Add New Msg</a></p>
-<hr />
-<table width="200" border="1">
-  <tr>
-    <td>id</td>
-    <td>title</td>
-    <td>message</td>
-    <td>name</td>
-  </tr>
-<?php
-while (	$rs=mysqli_fetch_assoc($result)) {
-	echo "<tr><td>" . $rs['id'] . "</td>";
-	echo "<td>{$rs['title']}</td>";
-	echo "<td>" , $rs['msg'], "</td>";
-	echo "<td>" . $rs['name'] . "</td></tr>";
-}
-?>
-</table>
+    <p>my guest book !! <a href='addMessageForm.php' target="_blank">Add New Msg</a></p>
+    <button onclick='reloadPage()'>Reload</button>
+    <hr />
+    <div id="main">
+        <table width="200" border="1">
+            <tr>
+                <td>id</td>
+                <td>title</td>
+                <td>message</td>
+                <td>name</td>
+            </tr>
+            <?php
+            while ($rs = mysqli_fetch_assoc($result)) {
+                echo "<tr><td>" . $rs['id'] . "</td>";
+                echo "<td>{$rs['title']}</td>";
+                echo "<td>", $rs['msg'], "</td>";
+                echo "<td>" . $rs['name'] . "</td></tr>";
+            }
+            ?>
+        </table>
+    </div>
+    <script>
+        //settimeout example
+        function reloadPage() {
+            // window.location.reload();	
+            fetch()......
+                // 1 秒後，自動執行一次
+                setTimeout(reloadPage, 1000);
+        }
+        setTimeout(reloadPage, 1000);
+
+        //setinterval example
+        function reloadData() {
+            //window.location.reload();	
+            fetch()......
+        }
+        setInterval(reloadData, 3500);
+    </script>
 </body>
+
 </html>
