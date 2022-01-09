373dd710d321a8c3e0f7a7a440fb86a26290a7a8
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-01-10 00:17:25 +0800

    vue ajax

--- vue/base.js
+++ vue/base.js
@@ -1,5 +1,7 @@
 function baseOBJ() {
+	// 宣告一整個 function 層級的變數
 	var baseApp;
+	// 定義一個模板
 	const template=`
 <form id='baseForm'>
 編號<input type='text' v-model='dat.id' disabled ><br>
@@ -14,13 +16,19 @@ function baseOBJ() {
 </form>
 `;
 	
+	// 宣告的函數可以透過 this 去取用，變成 public function
 	this.loadRecord=function (id,div) {
+		// let 宣告區塊層級的變數，有效層只有在這個 block 裡面
 		let elmnt = document.getElementById(div);
 		/*search for elements with a certain atrribute:*/
 		if (elmnt) {
+			// 把模板的資料塞進 elmnt
 			elmnt.innerHTML = template;
 			let url="getData.php";
+			
+			// 用 GET 的方式傳給後台
 			fetch(url+"?id=" + id.toString())
+			// 把 promise 物件轉成 json 物件，然後再傳給下一個 call back function
 			.then(function(resp){return resp.json();})
 			.then(function(json) {
 					//console.log(json.group_no);
@@ -30,10 +38,12 @@ function baseOBJ() {
 							return {
 								dat: json,
 								xid: id,
+								// 定義選單的選項
 								litOpts: [{v:1,o:'a'},{v:2,o:'b'},{v:3,o:'c'}]
 							}
 						},
 						methods: {
+							// 把使用者在表單填的東西，包在一個虛擬表單，傳給後台
 							submitit() {									
 								let mydat = new FormData();
 								//console.log(this.dat);
@@ -54,6 +64,7 @@ function baseOBJ() {
 									method: 'POST', 
 									body: mydat									
 								})
+								// 後臺處理完訊息後，傳回來
 								.then(function(res){return res.text(); })
 								.then(function(data){ 
 									console.log(data)
@@ -61,6 +72,7 @@ function baseOBJ() {
 							}
 						}
 					});
+					// 把剛剛宣告的物件 (baseApp)，綁定到 div 上
 					baseApp.mount("#"+div);
 				})
 		} //end if
